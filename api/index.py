a8154c1618d7bd1cea1c38f024364a5c6e8d03f4
Janine Rivera <2022371117@uteq.edu.mx>
2026-08-19 00:27:18 -0600

    Delete api directory

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-
-from app import app  # noqa: E402
-
-# Vercel busca una variable llamada "app" (WSGI) en este módulo.
